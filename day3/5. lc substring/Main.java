1bd9150cd2a0955a570513368a283c1455b5aba3
Shubham Singh Bhaisora <shubhamsb@outlook.com>
2023-01-08 14:17:02 +0000

    day 3 longest common substring

--- day3/5. lc substring/Main.java
+++ day3/5. lc substring/Main.java
@@ -9,51 +9,47 @@ public class Main {
     StringTokenizer st;
     StringBuilder sb = new StringBuilder();
 
-    char[] a, b, c;
-    int p, q, r;
-    int[][][] dp = new int[100][100][100];
-
+    char[] a,b;
+    int n,m;
+    
+    int[][] dp=new int[1000][1000];
+    
     public void solve() throws IOException {
+        for(int[] a:dp)
+            Arrays.fill(a,-1);
+        
         st = new StringTokenizer(br.readLine());
-        a = st.nextToken().toCharArray();
-        b = st.nextToken().toCharArray();
-        c = st.nextToken().toCharArray();
-
-        p = a.length;
-        q = b.length;
-        r = c.length;
-
-        for (int[][] a : dp)
-            for (int[] b : a)
-                Arrays.fill(b, -1);
-
-        int ans = lcs(0, 0, 0);
-        sb.append(ans).append("\n");
+        a=st.nextToken().toCharArray();
+        b=st.nextToken().toCharArray();
+        n=a.length;
+        m=b.length;
+        
+        int max=0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++){
+                max=Math.max(max,rec(i,j));
+            }
+        
+        sb.append(max).append("\n");
     }
-
-    private int lcs(int i, int j, int k) {
-        if (i == p || j == q || k == r) {
+    
+    private int rec(int x,int y){ //longest common substring starting at x and y
+        if(x==n || y==m){
             return 0;
         }
-
-        if (dp[i][j][k] != -1)
-            return dp[i][j][k];
-
-        int ans = 0;
-        // move ptr for a
-        ans = Math.max(ans, lcs(i + 1, j, k));
-        // move ptr for b
-        ans = Math.max(ans, lcs(i, j + 1, k));
-        // move ptr for c
-        ans = Math.max(ans, lcs(i, j, k + 1));
-
-        if (a[i] == b[j] && b[j] == c[k]) {
-            ans = Math.max(ans, 1 + lcs(i + 1, j + 1, k + 1));
+        if(dp[x][y]!=-1)
+            return dp[x][y];
+        
+        int ans=0;
+        if(a[x]==b[y]){
+            ans = 1+rec(x+1,y+1);
         }
-
-        return dp[i][j][k] = ans;
+        else{
+            ans = 0;
+        }
+        return dp[x][y]=ans;
     }
-
+    
     private void swap(int[] ar, int i, int j) {
         int temp = ar[i];
         ar[i] = ar[j];

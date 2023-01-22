095052acf7db51490bca2f320a91becb9511b7b1
Shubham Singh Bhaisora <shubhamsb@outlook.com>
2023-01-22 20:48:24 +0530

    [atcoder] coins

---
+++ atcoder solutions/I. coins/Main.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    int n;
+    double[] prob;
+    
+    double[][] dp;
+
+    public void solve() throws IOException {
+        st=new StringTokenizer(br.readLine());
+
+        n=Integer.parseInt(st.nextToken());
+        
+        prob=new double[n];
+        
+        dp=new double[n][n];
+        
+        for(double[] a:dp)
+            Arrays.fill(a,-1.0d);
+        
+        st=new StringTokenizer(br.readLine());
+        
+        for(int i=0;i<n;i++){
+            prob[i]=Double.parseDouble(st.nextToken());
+        }
+        
+        sb.append(rec(0,0));
+    }
+    
+    private double rec(int x,int heads){ // probability of getting more heads than tails from [x..n]
+        if(x==n){
+            int tails=x-heads;
+            if(heads>tails){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        
+        if(dp[x][heads]!=-1)
+            return dp[x][heads];
+        
+        double ans=0.0;
+        //taking head
+        ans+=(prob[x]*rec(x+1,heads+1));
+        //taking tails
+        ans+=(1.0-prob[x])*rec(x+1,heads);
+        
+        return dp[x][heads]=ans;
+    }
+
+    private void swap(int[] ar, int i, int j) {
+        int temp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = temp;
+    }
+
+    private class Pair {
+        int x;
+        int y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
+    private void runner(int t) throws IOException {
+        while (t != 0) {
+            solve();
+            t--;
+        }
+        System.out.println(sb);
+    }
+
+    public void multipleCaseRunner() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(st.nextToken());
+
+        runner(t);
+    }
+
+    public void singleCaseRunner() throws IOException {
+        runner(1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.singleCaseRunner();
+        // main.multipleCaseRunner();
+    }
+}
